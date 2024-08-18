c0a6402187201553465dab9e10fd0b59bb2dbd66
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-18 21:29:38 +0700

    feat: add user authentication

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,26 +1,41 @@
-import { auth } from "~/auth";
+"use client";
+
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-export default async function ProfilePage() {
-  const session = await auth();
+export default function Profile() {
+  const router = useRouter();
+  const { data } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/api/auth/signin");
+    },
+  });
 
-  const user = session?.user;
+  const user = data?.user;
 
   return (
-    <div className="flex items-center gap-8">
-      <div>
-        <Image
-          src={user?.image ? user.image : "/images/default.png"}
-          alt={`profile photo of ${user?.name}`}
-          width={90}
-          height={90}
-        />
-      </div>
-      <div className="mt-8">
-        <p className="mb-3">ID: {user?.id}</p>
-        <p className="mb-3">Name: {user?.name}</p>
-        <p className="mb-3">Email: {user?.email}</p>
-      </div>
-    </div>
+    <>
+      {!user ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="flex items-center gap-8">
+          <div>
+            <Image
+              src={user.image ? user.image : "/images/default.png"}
+              alt={`profile photo of ${user.name}`}
+              width={90}
+              height={90}
+            />
+          </div>
+          <div className="mt-8">
+            <p className="mb-3">ID: {user.id}</p>
+            <p className="mb-3">Name: {user.name}</p>
+            <p className="mb-3">Email: {user.email}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
