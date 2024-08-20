6d314be486d1e21657356abbdb076e8c8bb40dc9
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-20 19:39:03 +0700

    feat: add authenticated user router

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,6 +1,6 @@
 import { hash, hashSync } from "bcryptjs";
 import { and, eq } from "drizzle-orm";
-
+import { createHash } from "crypto";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -8,18 +8,18 @@ import { users } from "~/server/db/schema";
 
 export const userRouter = createTRPCRouter({
 
-
-
   register: publicProcedure
     .input(z.object({ name: z.string().min(1),email: z.string().email(),password: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
+      const pw = createHash('sha256').update(input.password).digest('hex');
 
-      const pw = hashSync(input.password,10);   
-      return await ctx.db.insert(users).values({
+      await ctx.db.insert(users).values({
         name: input.name,
         email: input.email,
         password: pw
       });
+
+      return{ message: "new user successfully created"}
     }),
 
   authenticateUser: publicProcedure
@@ -31,16 +31,16 @@ export const userRouter = createTRPCRouter({
   ))
   .query(async ({ ctx , input }) => {
 
-    const pw = hashSync(input.password,10);
-
-    return await ctx.db
-        .select()
-        .from(users)
-        .where(
-            and(
-                eq(users.email,input.email),
-                eq(users.password,pw)
-            ))
+    const pw = createHash('sha256').update(input.password).digest('hex');
+    const user = await ctx.db
+    .select()
+    .from(users)
+    .where(
+        and(
+            eq(users.email,input.email),
+            eq(users.password,pw)
+        ))
+    return user[0]
   }),
   getAllUsers: publicProcedure.query(async ({ ctx }) => {
     // Expected output: seluruh data course yang ada
