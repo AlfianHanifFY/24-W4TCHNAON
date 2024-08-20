76f6e7db77f7bd2d65c4472cef26fa0581000a3a
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-20 09:31:30 +0700

    feat: add credential auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,72 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import NextAuth from 'next-auth';
 import github from 'next-auth/providers/github';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import Credentials from 'next-auth/providers/credentials';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import { db } from './server/db';
+import email from 'next-auth/providers/email';
+import credentials from 'next-auth/providers/credentials';
+
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   session: { strategy: 'jwt' },
   adapter: DrizzleAdapter(db),
+  pages : {
+    signIn : '/login'
+  },
   providers: [
     github({ allowDangerousEmailAccountLinking: true }),
-    CredentialsProvider({}),
+    Credentials({
+      credentials :{
+        email: {},
+        password : {}
+      },
+      authorize : async (credentials) => {
+        let user = null
+ 
+        // logic to salt and hash password
+        const pwHash = saltAndHashPassword(credentials.password)
+ 
+        // logic to verify if the user exists
+        user = await getUserFromDb(credentials.email, pwHash)
+
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // meaning this is also the place you could do registration
+          throw new Error("User not found.")
+        }
+ 
+        // return user object with their profile data
+        return user
+      }
+    })
   ],
-  callbacks: {},
+  callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as any;
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey,
+        };
+      }
+      return token;
+    },
+    session(params) {
+      return {
+        ...params.session,
+        user: {
+          ...params.session.user,
+          id: params.token.id as string,
+          randomKey: params.token.randomKey,
+        },
+      };
+    },
+   
+  },
 });
+
+
