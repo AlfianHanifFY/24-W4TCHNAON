683cd937895bda2b09baad44bdb53c4d2697497d
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-25 13:55:23 +0700

    feat: rename export type infer

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -219,6 +219,6 @@ export type MovieLanguages = typeof movieLanguages.$inferSelect;
 export type Account = typeof accounts.$inferSelect;
 export type Session = typeof sessions.$inferSelect;
 export type UserComment = typeof userComments.$inferSelect;
-export type UserFavourite = typeof userFavourite.$inferSelect;
+export type UserFavourite = typeof userFavorite.$inferSelect;
 export type UserWatchLater = typeof userWatchLater.$inferSelect;
 
