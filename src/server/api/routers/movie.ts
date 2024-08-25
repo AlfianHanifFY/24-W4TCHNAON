ce0001c8d38e49f06149f7a549e1f21c1e6a2e48
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-25 10:59:24 +0700

    feat : add movieDetail procedure

--- src/server/api/routers/movie.ts
+++ src/server/api/routers/movie.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { z } from "zod";
+import { eq } from "drizzle-orm";
+import { string, z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { movies,  posts } from "~/server/db/schema";
+import { movieActors, movieCountries, movieGenres, moviePosters, movieReleases, movies,  movieStudios,  movieThemes,  posts } from "~/server/db/schema";
 
 export const movieRouter = createTRPCRouter({
 
@@ -27,4 +28,53 @@ export const movieRouter = createTRPCRouter({
       // Get all courses
       return await ctx.db.select().from(movies).limit(5);
     }),
-});
+
+    getMovieDetail: publicProcedure
+      .input(z.object({ movieId: z.string() }))
+      .query(async ({ ctx, input }) => {
+
+      const genre = await ctx.db
+        .select()
+        .from(movieGenres)
+        .where(eq(movieGenres.movieId, input.movieId))
+
+      const detail = await ctx.db
+        .select()
+        .from(movies)
+        .where(eq(movies.id, input.movieId));
+
+      const country= await ctx.db
+        .select()
+        .from(movieCountries)
+        .where(eq(movieCountries.movieId, input.movieId))
+
+      const actor = await ctx.db
+        .select()
+        .from(movieActors)
+        .where(eq(movieActors.movieId, input.movieId))
+      
+      const poster = await ctx.db
+        .select()
+        .from(moviePosters)
+        .where(eq(moviePosters.movieId, input.movieId))
+
+      const release = await ctx.db
+        .select()
+        .from(movieReleases)
+        .where(eq(movieReleases.movieId, input.movieId))
+
+      const theme = await ctx.db
+        .select()
+        .from(movieThemes)
+        .where(eq(movieThemes.movieId, input.movieId))
+
+      const studio = await ctx.db
+        .select()
+        .from(movieStudios)
+        .where(eq(movieStudios.movieId, input.movieId))
+
+      return {detail,genre,country,actor,poster,release,theme,studio}
+    }),
+
+    
+  });
