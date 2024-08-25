01770ad42ee78d9abec8903bcc020e4ed6cc6a41
sonix03 <supersonix03@gmail.com>
2024-08-25 19:05:05 +0700

    new file:   src/app/ClientLayout.tsx 	modified:   src/app/layout.tsx 	new file:   src/app/navbar/page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import "~/styles/globals.css";
-
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
-
 import { TRPCReactProvider } from "~/trpc/react";
+import ClientLayout from "./ClientLayout";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -16,9 +15,13 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+      <body className="flex flex-col">
+        <TRPCReactProvider>
+          <ClientLayout>{children}</ClientLayout>
+        </TRPCReactProvider>
       </body>
     </html>
   );
 }
+
+
