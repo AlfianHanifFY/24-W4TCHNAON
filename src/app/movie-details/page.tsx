acf8fcc77cb1ffd1f9af755cba47cb6a0ca209f6
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-26 18:56:44 +0700

    chore: refactor page.tsx

--- src/app/movie-details/page.tsx
+++ src/app/movie-details/page.tsx
@@ -13,20 +13,5 @@ export default function Profile() {
   const router = useRouter();
   const { data } = useSession();
 
-  return (
-    <div>
-      <MovieDetailsTop userId={data?.user?.id} />
-      <div className="absolute left-20 right-6 z-10 mt-48 h-0.5 bg-[#757575]">
-        <div className="absolute mt-3 items-center justify-between rounded-full bg-[#B3B3B3] px-8 py-1 font-normal italic">
-          Similar to
-        </div>
-        <div className="absolute left-0 right-0 z-10 m-2 mt-14 h-0.5 bg-[#757575]"></div>
-        {/* Box */}
-        <MovieDetailsSimiliar />
-        <div className="absolute left-0 right-0 z-10 m-2 h-0.5 bg-[#757575]"></div>
-        {/* Comment */}
-        <MovieDetailsComment userId={data?.user?.id} />
-      </div>
-    </div>
-  );
+  return <div>kurang parameter oy</div>;
 }
