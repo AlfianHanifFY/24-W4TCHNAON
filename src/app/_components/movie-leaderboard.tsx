b665fd2a35ee02db6f0ef45cc19aedae377289b2
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-26 19:45:03 +0700

    feat: add movie route in leaderboard movie card

--- src/app/_components/movie-leaderboard.tsx
+++ src/app/_components/movie-leaderboard.tsx
@@ -31,11 +31,15 @@ export default function MovieLeaderboard() {
           <div className="absolute -right-5 -top-5 rounded-full bg-gray-400 px-3 py-1 text-white shadow-lg">
             2
           </div>
-          <img
-            src={data.data?.list[1].link}
-            alt="Cover"
-            className="h-56 w-full rounded-lg shadow-md"
-          />
+          <a
+            href={`http://localhost:3000/movie-details/${data.data?.list[1].id}`}
+          >
+            <img
+              src={data.data?.list[1].link}
+              alt="Cover"
+              className="h-56 w-full rounded-lg shadow-md"
+            />
+          </a>
           <div className="mt-4">
             <p className="text-lg font-bold">{data.data?.list[1].name}</p>
             <p className="text-gray-500">{data.data?.list[1].rating}</p>
@@ -45,11 +49,16 @@ export default function MovieLeaderboard() {
           <div className="absolute -right-6 -top-6 rounded-full bg-yellow-400 px-3 py-1 text-white shadow-lg">
             1
           </div>
-          <img
-            src={data.data?.list[0].link}
-            alt="Cover"
-            className="h-64 w-full rounded-lg shadow-md"
-          />
+          <a
+            href={`http://localhost:3000/movie-details/${data.data?.list[0].id}`}
+          >
+            <img
+              src={data.data?.list[0].link}
+              alt="Cover"
+              className="h-64 w-full rounded-lg shadow-md"
+            />
+          </a>
+
           <div className="mt-4">
             <p className="text-lg font-bold">{data.data?.list[0].name}</p>
             <p className="text-gray-500">{data.data?.list[0].rating}</p>
@@ -59,11 +68,16 @@ export default function MovieLeaderboard() {
           <div className="absolute -right-5 -top-5 rounded-full bg-orange-400 px-3 py-1 text-white shadow-lg">
             3
           </div>
-          <img
-            src={data.data?.list[2].link}
-            alt="Cover"
-            className="h-56 w-full rounded-lg shadow-md"
-          />
+          <a
+            href={`http://localhost:3000/movie-details/${data.data?.list[2].id}`}
+          >
+            <img
+              src={data.data?.list[2].link}
+              alt="Cover"
+              className="h-56 w-full rounded-lg shadow-md"
+            />
+          </a>
+
           <div className="mt-4">
             <p className="text-lg font-bold">{data.data?.list[2].name}</p>
             <p className="text-gray-500">{data.data?.list[2].rating}</p>
@@ -80,11 +94,14 @@ export default function MovieLeaderboard() {
             <span className="w-12 text-3xl font-bold text-gray-700">
               {rank + 4}
             </span>
-            <img
-              src={val.link}
-              alt="Cover"
-              className="ml-6 h-28 w-20 rounded-lg shadow-sm"
-            />
+            <a href={`http://localhost:3000/movie-details/${val.id}`}>
+              <img
+                src={val.link}
+                alt="Cover"
+                className="ml-6 h-28 w-20 rounded-lg shadow-sm"
+              />
+            </a>
+
             <div className="ml-6">
               <p className="text-lg font-bold">{val.name}</p>
               <p className="text-gray-500">{val.rating}</p>
