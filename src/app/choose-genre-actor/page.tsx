88e22f6d4b2c1d806059446b265480b8e90d6002
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-28 11:20:21 +0700

    feat: add user preference

--- src/app/choose-genre-actor/page.tsx
+++ src/app/choose-genre-actor/page.tsx
@@ -1,4 +1,145 @@
+/* eslint-disable react/jsx-key */
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { api } from "~/trpc/react";
+
 export default function Preference() {
+  const router = useRouter();
+  const { data, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/api/auth/signin");
+      window.location.reload();
+    },
+  });
+
+  const popularActors = [
+    "Leonardo DiCaprio",
+    "Margot Robbie",
+    "Ryan Gosling",
+    "Emma Stone",
+    "Brad Pitt",
+    "Tom Hanks",
+    "Joaquin Phoenix",
+    "Robert De Niro",
+    "Scarlett Johansson",
+    "Jennifer Lawrence",
+    "Robert Downey Jr.",
+    "Keanu Reeves",
+    "Jake Gyllenhaal",
+    "Cate Blanchett",
+    "Matthew McConaughey",
+    "Christian Bale",
+    "Cillian Murphy",
+    "Tom Cruise",
+    "Meryl Streep",
+    "George Clooney",
+  ];
+  const genres = [
+    "Comedy",
+    "Adventure",
+    "Thriller",
+    "Drama",
+    "Science Fiction",
+    "Action",
+    "Music",
+    "Romance",
+    "History",
+    "Crime",
+    "Animation",
+    "Mystery",
+    "Horror",
+    "Family",
+    "Fantasy",
+    "War",
+    "Western",
+    "TV Movie",
+    "Documentary",
+  ];
+  const countries = [
+    "UK",
+    "USA",
+    "South Korea",
+    "Germany",
+    "Hong Kong",
+    "Canada",
+    "Sweden",
+    "Ireland",
+    "Japan",
+    "China",
+    "France",
+    "Brazil",
+    "Italy",
+    "Czechia",
+    "New Zealand",
+    "Australia",
+    "India",
+    "Spain",
+    "Austria",
+    "Greece",
+    "Netherlands",
+    "Taiwan",
+    "Poland",
+    "Mexico",
+    "Finland",
+    "Denmark",
+    "Singapore",
+    "Switzerland",
+    "Chile",
+    "Belgium",
+    "United Arab Emirates",
+    "Malta",
+    "Hungary",
+    "South Africa",
+    "Turkey",
+    "Bulgaria",
+    "Argentina",
+    "Iceland",
+    "Norway",
+    "Indonesia",
+    "Slovenia",
+    "Iran",
+    "Luxembourg",
+    "Philippines",
+    "Malaysia",
+    "Portugal",
+    "Serbia",
+    "Romania",
+    "Colombia",
+    "Thailand",
+    "Cyprus",
+    "Lebanon",
+    "Qatar",
+    "Bahamas",
+    "Puerto Rico",
+    "Russian Federation",
+    "Cuba",
+    "Dominican Republic",
+    "Senegal",
+    "Bolivarian Republic of Venezuela",
+    "Morocco",
+    "Croatia",
+    "Guatemala",
+    "Latvia",
+    "Cambodia",
+    "Mongolia",
+    "Slovakia",
+    "Ukraine",
+    "Tunisia",
+    "Myanmar",
+  ];
+
+  const createUserActor = api.user.createUserActor.useMutation();
+  const createUserCountry = api.user.createUserCountry.useMutation();
+  const creaeUserGenre = api.user.createUserGenre.useMutation();
+  const user = data?.user;
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="mt-20 p-6 font-light">
       <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -16,109 +157,94 @@ export default function Preference() {
           Done
         </a>
       </div>
-      <div style={{ fontFamily: "Montserrat" }} className="text-sm">
-        SELECT AT LEAST 3
-      </div>
 
       {/* Actors and Actresses */}
-      <div className="space-y-4">
-        <div className="mt-5 flex flex-row justify-between px-10 text-center">
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor A</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor B</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor C</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor D</a>
-          </div>
+      <>
+        <div className="mt-4 grid grid-cols-4 gap-4 bg-white p-4">
+          {popularActors.map((val) => {
+            return (
+              <div className="m-2 rounded-full bg-white shadow-md hover:scale-105">
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    createUserActor.mutate({
+                      userId: user?.id,
+                      actor: val,
+                    });
+                  }}
+                >
+                  <div className="ml p-4">
+                    <div className="text-center font-semibold">
+                      <button>{val}</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            );
+          })}
         </div>
-        <div className="mt-5 flex flex-row justify-between px-10 text-center">
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor A</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor B</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor C</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor D</a>
-          </div>
-        </div>
-        <div className="mt-5 flex flex-row justify-between px-10 text-center">
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor A</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor B</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor C</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Actor D</a>
-          </div>
-        </div>
-      </div>
-
+      </>
       {/* Text Genre */}
       <div className="flex flex-row justify-between pt-8">
         <div className="text-2xl font-semibold">Your Favorite Genres</div>
       </div>
-      <div style={{ fontFamily: "Montserrat" }} className="text-sm">
-        SELECT AT LEAST 3
-      </div>
 
       {/* Genre */}
-      <div className="space-y-4">
-        <div className="mt-5 flex flex-row justify-between px-10 text-center">
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Romance</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Thriller</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Drama</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Comedy</a>
-          </div>
+      <>
+        <div className="mt-4 grid grid-cols-4 gap-4 bg-white p-4">
+          {genres.map((val) => {
+            return (
+              <div className="m-2 rounded-full bg-white shadow-md hover:scale-105">
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    creaeUserGenre.mutate({
+                      userId: user?.id,
+                      genre: val,
+                    });
+                  }}
+                >
+                  <div className="ml p-4">
+                    <div className="text-center font-semibold">
+                      <button>{val}</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            );
+          })}
         </div>
-        <div className="mt-5 flex flex-row justify-between px-10 text-center">
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Horror</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Action</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Animation</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Documenter</a>
-          </div>
-        </div>
-        <div className="mt-5 flex flex-row justify-between px-10 text-center">
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Fantasy</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Science Fiction</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Musical</a>
-          </div>
-          <div className="flex h-[73px] w-[247px] cursor-pointer items-center justify-center rounded-full bg-gradient-to-r from-[#BA0000] to-[#FF0000] font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-shadow delay-300 hover:from-[#a30303] hover:to-[#d40000]">
-            <a href="#">Historical</a>
-          </div>
-        </div>
+      </>
+
+      {/* Countries */}
+      <div className="flex flex-row justify-between pt-8">
+        <div className="text-2xl font-semibold">Your Favorite Country</div>
       </div>
+      <>
+        <div className="mt-4 grid grid-cols-4 gap-4 bg-white p-4">
+          {countries.map((val) => {
+            return (
+              <div className="m-2 rounded-full bg-white shadow-md hover:scale-105">
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    createUserCountry.mutate({
+                      userId: user?.id,
+                      country: val,
+                    });
+                  }}
+                >
+                  <div className="ml p-4">
+                    <div className="text-center font-semibold">
+                      <button>{val}</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            );
+          })}
+        </div>
+      </>
     </div>
   );
 }
