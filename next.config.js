e0caed213942fa18ae81c2a34cf6568de9abf815
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-28 19:33:33 +0700

    feat: change config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,12 @@ const config = {images: {
         hostname: 'avatars.githubusercontent.com',
       },
     ],
+  },typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },};
 
 export default config;
