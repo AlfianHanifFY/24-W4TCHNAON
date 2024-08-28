7d6f3982691b313d0a1816038bb19f753a4d3823
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-08-28 20:51:35 +0700

    chore:change config

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ const config = {images: {
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
+  },eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },};
 
 export default config;
