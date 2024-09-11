f22145e307a9bcbc83caa7c5cf8460aebd83c82b
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-09-11 11:05:23 +0700

    chore: change to black

--- src/app/recommendation/page.tsx
+++ src/app/recommendation/page.tsx
@@ -9,8 +9,8 @@ import { MovieRecomendationByLocation } from "../_components/movie-recomendation
 export default function Recommendation() {
   const mostPopularMovies = api.movie.getPopularMovieRecomendation.useQuery();
   return (
-    <div className="mt-8">
-      <h2 className="text-3xl font-bold text-gray-800">New Release Movies</h2>
+    <div className="mt-8 bg-black">
+      <h2 className="text-3xl font-bold text-white">New Release Movies</h2>
       <div className="mt-4 grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-6">
         {mostPopularMovies.data?.list.map((movie) => (
           <a href={`movie-details/${movie.id}`}>
@@ -23,7 +23,7 @@ export default function Recommendation() {
                 alt={movie.name}
                 className="mx-auto h-auto w-48 rounded-lg shadow-md"
               />
-              <h3 className="mx-auto mt-2 w-32 text-sm font-semibold text-gray-800">
+              <h3 className="mx-auto mt-2 w-32 text-sm font-semibold text-white">
                 {movie.name}
               </h3>
             </div>
